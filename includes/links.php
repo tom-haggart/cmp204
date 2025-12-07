b74c5fc81fba4ca53d7e24bd908506cf6fe4753a
Tom Haggart <tom.haggart@gmail.com>
2025-12-07 23:54:56 +0000

    feat: dynamically set the active primary nav link

--- includes/links.php
+++ includes/links.php
@@ -1,16 +1,24 @@
+<?php
+    $current_page = basename($_SERVER['PHP_SELF']);
+
+    function isActive($page, $current_page) {
+        return ($page === $current_page) ? 'class="nav-link active" aria-current="page"' : 'class="nav-link"';
+    }
+?>
+
 <ul class="nav nav-pills">
     <li class="nav-item">
-        <a href="index.php" class="nav-link active" aria-current="page">Dashboard</a>
+        <a href="index.php" <?php echo isActive("index.php", $current_page) ?>>Dashboard</a>
     </a>
     </li>
-    <li class="nav-item"><a href="history.php" class="nav-link">History</a></li>
-    <li class="nav-item"><a href="teamMembers.php" class="nav-link">Team Members</a></li>
+    <li class="nav-item"><a href="history.php" <?php echo isActive("history.php", $current_page) ?>>History</a></li>
+    <li class="nav-item"><a href="teamMembers.php" <?php echo isActive("teamMembers.php", $current_page) ?>>Team Members</a></li>
 
     <!--only show if user is not signed in-->
-    <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
-    <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+    <li class="nav-item"><a href="register.php" <?php echo isActive("register.php", $current_page) ?>>Register</a></li>
+    <li class="nav-item"><a href="login.php" <?php echo isActive("login.php", $current_page) ?>>Login</a></li>
 
     <!--only show if authenticated user is signed in-->
-    <li class="nav-item"><a href="userProfile.php" class="nav-link">User Profile</a></li>
+    <li class="nav-item"><a href="userProfile.php" <?php echo isActive("userProfile.php", $current_page) ?>>User Profile</a></li>
     <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
 </ul>

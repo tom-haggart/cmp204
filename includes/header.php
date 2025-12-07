a4122484c5ae1057dad231f335429de3a612966a
Tom Haggart <tom.haggart@gmail.com>
2025-12-07 23:00:43 +0000

    feat: improve nav and organise header, footer and html head

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-bottom border-custom-dark">
+    <div class="d-flex justify-content-center align-items-center">
+        <img src="img/logo.svg" width="40" class="me-3">
+        <div class="fs-4">Save Point Summit</div>
+    </div>
+    <?php include_once "includes/links.php" ?>
+</header>
--- includes/links.php
+++ includes/links.php
@@ -1,14 +1,17 @@
-<ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="history.php">History</a></li>
-    <li><a href="teamMembers.php">Team Members</a></li>
-    <li><a href="req.php">Requirements</a></li>
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a href="index.php" class="nav-link active" aria-current="page">Dashboard</a>
+    </a>
+    </li>
+    <li class="nav-item"><a href="history.php" class="nav-link">History</a></li>
+    <li class="nav-item"><a href="teamMembers.php" class="nav-link">Team Members</a></li>
+    <li class="nav-item"><a href="req.php" class="nav-link">Requirements</a></li>
 
     <!--only show if user is not signed in-->
-    <li><a href="register.php">Register</a></li>
-    <li><a href="login.php">Login</a></li>
+    <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+    <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
 
     <!--only show if authenticated user is signed in-->
-    <li><a href="userProfile.php">User Profile</a></li>
-    <li><a href="logout.php">Logout</a></li>
-</ul> 
+    <li class="nav-item"><a href="userProfile.php" class="nav-link">User Profile</a></li>
+    <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
+</ul>
